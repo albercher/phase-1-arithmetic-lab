428ca13b93ef7d7176e6bd3957d239a603959ec8
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-01 12:15:48 -0400

    commit tree 67459112ba970af31ac53aefc03a72816b0dda30

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1 ;
+
+const num3 = 19;
+const num4 = 5;
+var mod = num3 % num4;
+
+const max = Math.max(1, 20, 18, 12, 2);
